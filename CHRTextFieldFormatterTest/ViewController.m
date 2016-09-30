cb8cab65933c5a9b15e8d6889d6e69da3bfad8d7
Ricardo Barroso <ricardo.barroso@gmail.com>
2016-09-30 18:48:52 +0100

    Added original test text fields (copied from the "CHRTextFieldFormatter") example. Added new text fields for testing the customisation of the phone, SSN and business EIN number text fields.

--- CHRTextFieldFormatterTest/ViewController.m
+++ CHRTextFieldFormatterTest/ViewController.m
@@ -7,23 +7,39 @@
 //
 
 #import "ViewController.h"
+#import "CHRTextFieldFormatter.h"
+#import "CHRTextFieldFormatter.h"
+#import "CHRPhoneNumberMask.h"
+#import "CHRCardNumberMask.h"
 
 @interface ViewController ()
+@property (nonatomic, strong) IBOutlet UITextField *originalPhoneNumberTextField;
+@property (nonatomic, strong) IBOutlet UITextField *originalCardNumberTextField;
+@property (nonatomic, strong) IBOutlet UITextField *phoneNumberTextField;
+@property (nonatomic, strong) IBOutlet UITextField *ssnNumberTextField;
+@property (nonatomic, strong) IBOutlet UITextField *businessEINNumberTextField;
 
+@property (nonatomic, strong) CHRTextFieldFormatter *originalPhoneNumberFormatter;
+@property (nonatomic, strong) CHRTextFieldFormatter *originalCardNumberFormatter;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.originalPhoneNumberFormatter = [[CHRTextFieldFormatter alloc] initWithTextField:self.originalPhoneNumberTextField mask:[CHRPhoneNumberMask new]];
+    self.originalCardNumberFormatter = [[CHRTextFieldFormatter alloc] initWithTextField:self.originalCardNumberTextField mask:[CHRCardNumberMask new]];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if (textField == self.phoneNumberTextField) {
+        return [self.originalPhoneNumberFormatter textField:textField shouldChangeCharactersInRange:range replacementString:string];
+    } else if (textField == self.originalCardNumberTextField) {
+        return [self.originalCardNumberFormatter textField:textField shouldChangeCharactersInRange:range replacementString:string];
+    } else {
+        return YES;
+    }
 }
-
 
 @end
