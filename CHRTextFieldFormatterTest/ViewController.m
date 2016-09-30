b2a816e0da5b6957dd8862b1057aafdaa3896176
Ricardo Barroso <ricardo.barroso@gmail.com>
2016-09-30 18:25:40 +0100

    Initial Xcode project files.

---
+++ CHRTextFieldFormatterTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CHRTextFieldFormatterTest
+//
+//  Created by Ricardo Barroso on 30/09/2016.
+//  Copyright © 2016 Ricardo Barroso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ CHRTextFieldFormatterTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHRTextFieldFormatterTest
+//
+//  Created by Ricardo Barroso on 30/09/2016.
+//  Copyright © 2016 Ricardo Barroso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
