039cf3c03874e1c1d33e25c0800046a60436a384
Ricardo Barroso <ricardo.barroso@gmail.com>
2016-09-30 18:25:40 +0100

    Initial Xcode project files.

---
+++ CHRTextFieldFormatterTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHRTextFieldFormatterTest
+//
+//  Created by Ricardo Barroso on 30/09/2016.
+//  Copyright © 2016 Ricardo Barroso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
